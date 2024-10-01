58606ea655d353c9ce8409b1c75dd040a810faff
Esteban Gonzalez drummes.12@gmail.com <drummes.12@gmail.com>
2024-09-30 22:47:20 -0500

    Add new components for navigation, hero section, and specifications in PrimeFit. Configure TypeScript paths for cleaner imports

---
+++ src/consts/sections.ts
@@ -0,0 +1,26 @@
+export const SECTIONS = [
+  {
+    title: 'Especificaciones',
+    href: '/#especificaciones',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: 'Entrenador',
+    href: '/#entrenador',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: 'FAQ',
+    href: '/#faq',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    title: 'Contacto',
+    href: '/#contacto',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  }
+]
