fda049503a15467d75a9449cc9c6ef9acc5ed8a9
Esteban Gonzalez drummes.12@gmail.com <drummes.12@gmail.com>
2024-10-12 11:54:50 -0500

    Implement marquee component and integrate it into trainer section; add animation to theme for enhanced visual appeal

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee var(--duration) linear infinite'
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(calc(-100% - var(--gap)))' }
+        }
+      },
       colors: {
         'primary-color': '#e46c2b',
         'secondary-color': '#31382E',
