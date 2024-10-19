4746da9c389683fb7ac59942778e4baf1d6ae30d
Esteban Gonzalez drummes.12@gmail.com <drummes.12@gmail.com>
2024-10-19 12:45:13 -0500

    Refactor navbar section from 'Especificaciones' to 'Planes'; add new plans component and constants for displaying plans options

---
+++ src/consts/plans.ts
@@ -0,0 +1,33 @@
+export const PLANS = [
+  {
+    title: 'PrimeFit Plus',
+    description:
+      'PrimeFit es un plan de 8 sesiones por mes para que transformas tu vida.',
+    cost: {
+      value: '8',
+      unit: 'sesiones'
+    },
+    specs: ['Entrenador', 'Nutritionista', 'Desafío']
+  },
+  {
+    title: 'PrimeFit',
+    description:
+      'PrimeFit es un plan que te permite explotar tus potenciales y encontrar la forma de vivir que realmente te guste.',
+    cost: {
+      value: 'Reto'
+      // unit: 'USD'
+    },
+    specs: ['Entrenador', 'Nutritionista', 'Desafío'],
+    popular: true
+  },
+  {
+    title: 'PrimeFit Pro',
+    description:
+      'PrimeFit es un plan de 12 sesiones por mes para que transformas tu vida.',
+    cost: {
+      value: '12',
+      unit: 'sesiones'
+    },
+    specs: ['Entrenador', 'Nutritionista', 'Desafío']
+  }
+]
--- src/consts/sections.ts
+++ src/consts/sections.ts
@@ -1,7 +1,7 @@
 export const SECTIONS = [
   {
-    title: 'Especificaciones',
-    href: '/#especificaciones',
+    title: 'Planes',
+    href: '/#planes',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
   },
