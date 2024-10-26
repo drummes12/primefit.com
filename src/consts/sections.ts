c4a697256808986a0e034f0076d943a68effa562
Esteban Gonzalez drummes.12@gmail.com <drummes.12@gmail.com>
2024-10-25 22:14:16 -0500

    Feat text in description of sections

--- src/consts/sections.ts
+++ src/consts/sections.ts
@@ -3,24 +3,24 @@ export const SECTIONS = [
     title: 'Planes',
     href: '/#planes',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      'PrimeFit te brinda planes de entrenamiento personalizados y nutrición para mejorar tu salud y bienestar. ¡Elige el plan que mejor se adapte a tus necesidades y objetivos!'
   },
   {
     title: 'Entrenador',
     href: '/#entrenador',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      'PrimeFit cuenta con un entrenador profesional capacitado para ayudarte a alcanzar tus objetivos de salud y bienestar. ¡Conéctate hoy y empieza tu camino hacia una vida más activa y saludable!'
   },
   {
     title: 'FAQ',
     href: '/#faq',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      '¿Tienes preguntas sobre el entrenamiento personalizado o el plan de nutrición? ¡No dudes en preguntar! PrimeFit ofrece respuestas a tus preguntas y te ayuda a alcanzar tus objetivos de salud y bienestar.'
   },
   {
     title: 'Contacto',
     href: '/#contacto',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      '¿Tienes preguntas sobre el entrenamiento personalizado o el plan de nutrición? ¡No dudes en preguntar! PrimeFit ofrece respuestas a tus preguntas y te ayuda a alcanzar tus objetivos de salud y bienestar.'
   }
 ]
