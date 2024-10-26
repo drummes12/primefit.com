0a872bdaf78b6f2fb1f38458e4ca9e62f991eb6e
Esteban Gonzalez drummes.12@gmail.com <drummes.12@gmail.com>
2024-10-25 20:55:08 -0500

    Refactor FAQ component to dynamically render questions and answers from a new FAQ constant for improved maintainability

---
+++ src/consts/faq.ts
@@ -0,0 +1,34 @@
+export const FAQ = [
+  {
+    question: '¿Qué es un plan de entrenamiento personalizado?',
+    answer:
+      'es un programa de ejercicios diseñado específicamente para adaptarse a las necesidades, objetivos y condición física de una persona. Puede incluir ejercicios de fuerza, cardio, flexibilidad, entre otros.'
+  },
+  {
+    question: '¿Cuánto tiempo se tarda en ver resultados?',
+    answer:
+      'Los resultados pueden variar según la persona, sus objetivos y el nivel de compromiso. Algunas personas pueden notar cambios en unas pocas semanas, mientras que otras pueden necesitar algunos meses.'
+  },
+  {
+    question:
+      '¿Puedo hacer un plan de entrenamiento personalizado si tengo lesiones o condiciones médicas?',
+    answer:
+      'Sí, pero es importante informar al entrenador sobre cualquier lesión o condición médica. El plan se adaptará para evitar movimientos que puedan empeorar la situación y para incluir ejercicios que ayuden en la recuperación'
+  },
+  {
+    question: '¿Cuánto tiempo debería durar cada sesión de entrenamiento?',
+    answer:
+      'Generalmente, una sesión de entrenamiento puede durar entre 30 y 60 minutos, dependiendo del objetivo del programa y del nivel de condición física del individuo'
+  },
+  {
+    question:
+      '¿Cómo se mide el progreso con un plan de entrenamiento personalizado?',
+    answer:
+      'El progreso se puede medir mediante varios indicadores: cambios en la composición corporal (peso, porcentaje de grasa), aumento de fuerza, mejoras en el rendimiento cardiovascular'
+  },
+  {
+    question: '¿Es adecuado para todos los niveles de condición física?',
+    answer:
+      'Algunos retos están diseñados para principiantes, mientras que otros son para niveles intermedios o avanzados. Es importante elegir un reto que se adapte a tu nivel actual y capacidades.'
+  }
+]
