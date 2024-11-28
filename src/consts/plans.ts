03b3e6e84dbd052432dc8548ca00b3b16ae44d5a
Esteban Gonzalez drummes.12@gmail.com <drummes.12@gmail.com>
2024-11-27 20:14:39 -0500

    Refactor plans component structure: replace SendInfo with PlanInfo, update plan details, and enhance description visibility

--- src/consts/plans.ts
+++ src/consts/plans.ts
@@ -1,120 +1,111 @@
 export const PLANS = [
   {
-    id: 'primefit-plus',
-    title: 'PrimeFit Plus',
+    id: 'level-up',
+    title: 'Level Up 2M',
     description:
-      '¿Quieres mejorar tu salud y forma física sin dejar tu hogar? PrimeFit es tu camino hacia una vida más activa y saludable.',
+      '¿Estás listo para dar el primer paso hacia tu mejor versión y superar tus límites? ¡Es tu momento de brillar y transformar tu vida!',
     cost: {
-      value: '8',
-      unit: 'sesiones'
+      value: 'Reto'
+    },
+    specs: [
+      '8 semanas de entrenamiento',
+      'Videos explicativos',
+      'Consejos de nutrición',
+      '10% off en MasterFit'
+    ],
+    info: {
+      title: 'Level Up 2M',
+      subtitle: 'Reto',
+      description:
+        '¿Estás listo para dar el primer paso hacia tu mejor versión y superar tus límites? Este reto está diseñado para mejorar tu condición física general, reducir el riesgo de enfermedades crónicas y ayudarte a bajar de peso, ya sea en casa o gimnasio, llevando tu fuerza, energía y confianza al siguiente nivel. ¡Es tu momento de brillar y transformar tu vida!',
+      specs: [
+        '8 semanas de entrenamiento personalizado',
+        'Videos explicativos y demostrativos',
+        'Consejos para una alimentación saludable',
+        '10% off en el plan MasterFit si finalizas el reto'
+      ],
+      form: [
+        // {
+        //   name: 'nombre',
+        //   label: 'Tu Nombre',
+        //   type: 'text',
+        //   placeholder: 'PrimeFit',
+        //   required: true
+        // },
+        // {
+        //   name: 'email',
+        //   label: 'Tu Correo',
+        //   type: 'email',
+        //   placeholder: 'prime@fit.com',
+        //   required: true
+        // },
+        // {
+        //   name: 'phone_number',
+        //   label: 'Tu Número de Teléfono',
+        //   type: 'tel',
+        //   placeholder: '+57 (300) 123-4567',
+        //   required: true
+        // }
+      ]
+    }
+  },
+  {
+    id: 'powerfit',
+    title: 'PowerFit',
+    description:
+      '¿Y si con solo 2 días a la semana pudieras comenzar a transformar tu cuerpo y tu bienestar? ¡Es tu momento de empezar!',
+    cost: {
+      value: '2',
+      unit: 'por semana'
     },
     specs: [
       'Clase de cortesía',
-      'Diagnóstico completo',
-      'Alimentación',
+      'Diagnóstico físico completo',
+      'Consejos de nutrición',
       'Acompañamiento completo'
     ],
-    form: [
-      {
-        name: 'nombre',
-        label: 'Tu Nombre',
-        type: 'text',
-        placeholder: 'PrimeFit',
-        required: true
-      },
-      {
-        name: 'email',
-        label: 'Tu Correo',
-        type: 'email',
-        placeholder: 'prime@fit.com',
-        required: true
-      },
-      {
-        name: 'phone_number',
-        label: 'Tu Número de Teléfono',
-        type: 'tel',
-        placeholder: '+57 (300) 123-4567',
-        required: true
-      }
-    ]
-  },
-  {
-    id: 'primefit',
-    title: 'PrimeFit',
-    description:
-      '¿Quieres un reto personalizado para mejorar tu salud y forma física? PrimeFit es tu camino hacia una vida más activa y saludable.',
-    cost: {
-      value: 'Reto'
-      // unit: 'USD'
+    info: {
+      title: 'PowerFit',
+      subtitle: '2 veces a la semana',
+      description:
+        '¿Y si con solo 2 días a la semana pudieras comenzar a transformar tu cuerpo y tu bienestar? Este plan es la oportunidad perfecta para dar pasos consistentes hacia tus metas, ya sea de forma presencial o virtual, con entrenamientos efectivos, prácticos y diseñados para ajustarse a tu estilo de vida y objetivos. ¡Es tu momento de empezar!',
+      specs: [
+        'Clase de cortesía',
+        'Diagnóstico físico completo',
+        'Consejos para una alimentación saludable',
+        'Acompañamiento completo presencial y virtual (WhatsApp)'
+      ],
+      form: []
     },
-    specs: [
-      '8 Semanas de entrenamiento',
-      'Videos explicativos y demostrativos',
-      'Plan y consejos de nutrición',
-      'Acceso a comunidad de apoyo'
-    ],
-    form: [
-      {
-        name: 'nombre',
-        label: 'Tu Nombre',
-        type: 'text',
-        placeholder: 'PrimeFit',
-        required: true
-      },
-      {
-        name: 'email',
-        label: 'Tu Correo',
-        type: 'email',
-        placeholder: 'prime@fit.com',
-        required: true
-      },
-      {
-        name: 'phone_number',
-        label: 'Tu Número de Teléfono',
-        type: 'tel',
-        placeholder: '+57 (300) 123-4567',
-        required: true
-      }
-    ],
     popular: true
   },
   {
-    id: 'primefit-pro',
-    title: 'PrimeFit Pro',
+    id: 'masterfit',
+    title: 'MasterFit',
     description:
-      '¿Quieres mejorar tu salud y forma física sin dejar tu hogar? PrimeFit es tu camino hacia una vida más activa y saludable.',
+      '¿Estás listo para un cambio real? ¡El momento es ahora, no lo pienses más!',
     cost: {
-      value: '12',
-      unit: 'sesiones'
+      value: '3',
+      unit: 'por semana'
     },
     specs: [
       'Clase de cortesía',
-      'Diagnóstico completo',
-      'Alimentación',
-      'Acompañamiento completo'
+      'Diagnóstico físico completo',
+      'Dieta recomendada',
+      'Rutinas adicionales'
     ],
-    form: [
-      {
-        name: 'nombre',
-        label: 'Tu Nombre',
-        type: 'text',
-        placeholder: 'PrimeFit',
-        required: true
-      },
-      {
-        name: 'email',
-        label: 'Tu Correo',
-        type: 'email',
-        placeholder: 'prime@fit.com',
-        required: true
-      },
-      {
-        name: 'phone_number',
-        label: 'Tu Número de Teléfono',
-        type: 'tel',
-        placeholder: '+57 (300) 123-4567',
-        required: true
-      }
-    ]
+    info: {
+      title: 'MasterFit',
+      subtitle: '3 veces a la semana',
+      description:
+        '¿Estás listo para un cambio real? Con 3 días de entrenamiento a la semana, este plan está diseñado para desafiarte, impulsarte y llevarte al siguiente nivel. Ya sea de forma presencial o virtual, es intenso, transformador y hecho para quienes están decididos a alcanzar sus metas. ¡El momento es ahora, no lo pienses más!',
+      specs: [
+        'Clase de cortesía',
+        'Diagnóstico físico completo',
+        'Dieta recomendada para un peso ideal',
+        'Rutinas adicionales'
+      ],
+      form: []
+    }
   }
 ]
